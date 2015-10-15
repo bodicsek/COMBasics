ee9b462216e8cf71390db611dd415d6a3c9d2d9f
David Nabraczky <david.nabraczky@gmail.com>
2015-10-15 20:57:34 +0200

    Code cleanup.

--- AtlComDll/Component1.h
+++ AtlComDll/Component1.h
@@ -2,12 +2,7 @@
 
 #pragma once
 #include "resource.h"       // main symbols
-
-
-
 #include "AtlComDll_i.h"
-
-
 
 #if defined(_WIN32_WCE) && !defined(_CE_DCOM) && !defined(_CE_ALLOW_SINGLE_THREADED_OBJECTS_IN_MTA)
 #error "Single-threaded COM objects are not properly supported on Windows CE platform, such as the Windows Mobile platforms that do not include full DCOM support. Define _CE_ALLOW_SINGLE_THREADED_OBJECTS_IN_MTA to force ATL to support creating single-thread COM object's and allow use of it's single-threaded COM object implementations. The threading model in your rgs file was set to 'Free' as that is the only threading model supported in non DCOM Windows CE platforms."
@@ -15,9 +10,7 @@
 
 using namespace ATL;
 
-
 // Component1
-
 class ATL_NO_VTABLE Component1 :
   public CComObjectRootEx<CComSingleThreadModel>,
   public CComCoClass<Component1, &CLSID_Component1>,
@@ -35,8 +28,6 @@ public:
   BEGIN_COM_MAP(Component1)
     COM_INTERFACE_ENTRY(IKnowledgeBase)
   END_COM_MAP()
-
-
 
   DECLARE_PROTECT_FINAL_CONSTRUCT()
 
